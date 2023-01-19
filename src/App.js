e66f047ddcb56dcf490a88f5bc18b2d3d528968d
Karita <tejc190103@gmail.com>
2023-01-19 09:52:56 +0900

    新デザイン＆背景追加

---
+++ src/App.js
@@ -0,0 +1,222 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import "./App.css";
+
+//後でURL追加？
+export default function ActionAreaCard() {
+  return (
+    <><Card style={{
+              display:'inline-block'
+    }}
+      sx={{ maxWidth: 330 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="180"
+          image="https://i2.wp.com/sozaikoujou.com/wordpress/wp-content/uploads/2015/04/th_beef-fish_ca_021.png?w=660&ssl=1"
+          alt="ika" />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Hogeさん
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card><Card style={{
+              display:'inline-block'
+    }}
+     sx={{ maxWidth: 330 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="180"
+            image="https://1.bp.blogspot.com/-rC07Xpawh8o/WCqdfy4o3zI/AAAAAAAA_h8/4vcKiwkPTWQFym846gXR9JXX4Uc0H0DHwCLcB/s800/food_ika_ichiyaboshi.png"
+            alt="ika" />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Fooさん
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        </Card><Card style={{
+              display:'inline-block'
+    }}
+     sx={{ maxWidth: 330 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="180"
+            image="https://lh3.googleusercontent.com/proxy/2qSOpVsAWvldAIE_EAkQqhLuCTBchURy-i_KEiqsnaGBiHswdlPlIKL8zkpga64XeRR9pvqCu6YflU-b0xHfnHgXHsx6ZPXoDWSbu9uNuf-9T5ol=s0-d"
+            alt="ika" />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Aさん
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        </Card><Card style={{
+              display:'inline-block'
+    }}
+     sx={{ maxWidth: 330 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="180"
+            image="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/56654/squid-emoji-clipart-md.png"
+            alt="ika" />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Bさん
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        </Card><Card style={{
+              display:'inline-block'
+    }}
+     sx={{ maxWidth: 330 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="180"
+            image="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/56654/squid-emoji-clipart-md.png"
+            alt="ika" />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Bさん
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        </Card><Card style={{
+              display:'inline-block'
+    }}
+     sx={{ maxWidth: 330 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="180"
+            image="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/56654/squid-emoji-clipart-md.png"
+            alt="ika" />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Bさん
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        </Card><Card style={{
+              display:'inline-block'
+    }}
+     sx={{ maxWidth: 330 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="180"
+            image="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/56654/squid-emoji-clipart-md.png"
+            alt="ika" />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Bさん
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        </Card><Card style={{
+              display:'inline-block'
+    }}
+     sx={{ maxWidth: 330 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="180"
+            image="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/56654/squid-emoji-clipart-md.png"
+            alt="ika" />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Bさん
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        </Card><Card style={{
+              display:'inline-block'
+    }}
+     sx={{ maxWidth: 330 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="180"
+            image="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/56654/squid-emoji-clipart-md.png"
+            alt="ika" />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Bさん
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        </Card><Card style={{
+              display:'inline-block'
+    }}
+     sx={{ maxWidth: 330 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="180"
+            image="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/56654/squid-emoji-clipart-md.png"
+            alt="ika" />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Bさん
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        </Card><Card style={{
+              display:'inline-block'
+    }}
+     sx={{ maxWidth: 330 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="180"
+            image="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/56654/squid-emoji-clipart-md.png"
+            alt="ika" />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Bさん
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        </Card><Card style={{
+              display:'inline-block'
+    }}
+     sx={{ maxWidth: 330 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="180"
+            image="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/56654/squid-emoji-clipart-md.png"
+            alt="ika" />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Bさん
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        </Card><Card style={{
+              display:'inline-block'
+    }}
+     sx={{ maxWidth: 330 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="180"
+            image="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/56654/squid-emoji-clipart-md.png"
+            alt="ika" />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Bさん
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card></>
+  );
+}
